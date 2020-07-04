6427c5bb3d935dc12d354774194fe4e1ba70ec78
billboardeventz <67776431+billboardeventz@users.noreply.github.com>
2020-07-04 21:45:36 +0800

    xx

--- config.php
+++
@@ -1,9 +0,0 @@
-<?php
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "dentcare";
-$conn = new mysqli($servername, $username, $password, $dbname);
-if ($conn->connect_error){
-    die("Connection failed: " . $conn->connect_error);
-}?>	
